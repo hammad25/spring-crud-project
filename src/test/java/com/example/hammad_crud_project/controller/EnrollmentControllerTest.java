9b89626f9fcaab950f65bb99c656d95a31de0363
hammad25 <hammad2091504@hotmail.com>
2024-08-19 12:17:38 +0100

    initial commit

--- src/test/java/com/example/hammad_crud_project/controller/EnrollmentControllerTest.java
+++ src/test/java/com/example/hammad_crud_project/controller/EnrollmentControllerTest.java
@@ -1,11 +1,8 @@
 package com.example.hammad_crud_project.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 @WebMvcTest(EnrollmentController.class)
-public class EnrollmentControllerTest {
+public class EnrollmentControllerTest{
 
-    @Autowired
-    private MockMvc mockMvc;
 }
